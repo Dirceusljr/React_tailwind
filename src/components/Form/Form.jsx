20f7c0d0645e0ccbedb2239c8f6e2686bad3ed1f
Dirceusljr <dirceusljr@gmail.com>
2024-08-01 11:48:12 -0300

    feat: :sparkles: add: adicionando estilo ao form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,10 +8,10 @@ const Form = ({ onSubmit }) => {
         const email = event.target[1].value;
         onSubmit({ name, email });
     }
-  return <form onSubmit={safeSubmit}>
+  return <form onSubmit={safeSubmit} className='h-full flex flex-col items-center justify-center -mt-20 gap-10'>
     <input type="text" placeholder='Insira seu nome...' />
     <input type="email" placeholder='Insira seu email...' />
-    <button type="submit">Seguir</button>
+    <button type="submit" className='bg-alura-100 dark:bg-dark-200 text-gray-200 px-5 py-1 rounded-full uppercase'>Seguir</button>
   </form>
 }
 
