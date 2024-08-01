4839ae4c3e4b778f7ed75dc2cf12872bc9b4a520
Dirceusljr <dirceusljr@gmail.com>
2024-08-01 12:02:52 -0300

    feat: :sparkles: add: estilizando input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import Input from '../Input/Input';
 
 const Form = ({ onSubmit }) => {
     const safeSubmit = (event) => {
@@ -8,10 +8,10 @@ const Form = ({ onSubmit }) => {
         const email = event.target[1].value;
         onSubmit({ name, email });
     }
-  return <form onSubmit={safeSubmit} className='h-full flex flex-col items-center justify-center -mt-20 gap-10'>
-    <input type="text" placeholder='Insira seu nome...' />
-    <input type="email" placeholder='Insira seu email...' />
-    <button type="submit" className='bg-alura-100 dark:bg-dark-200 text-gray-200 px-5 py-1 rounded-full uppercase'>Seguir</button>
+  return <form onSubmit={safeSubmit} className='h-full flex flex-col items-center justify-center -mt-20 gap-10 mx-5'>
+    <Input type="text" required placeholder='Insira seu nome...' />
+    <Input type="email" required placeholder='Insira seu email...' />
+    <button type="submit" className='bg-alura-100 dark:bg-dark-200 text-gray-200 px-5 py-1 w-full max-w-sm rounded-full uppercase outline-none'>Seguir</button>
   </form>
 }
 
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Input = (props) => {
+  return <input className='py-1 pl-5 w-full max-w-lg rounded-full text-dark-200 dark:text-gray-200 dark:bg-dark-200 outline-none' {...props} />
+}
+
+export default Input
